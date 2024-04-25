915c7254cb87a42ceac103d77f213466084affb9
Hajar Aswad <hajaraswadkom@gmail.com>
2024-04-25 17:23:29 +0700

    add tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @viteReactRefresh
     @vite('resources/js/app.jsx')
+    @vite('resources/css/app.css')
     @inertiaHead
 </head>
 
