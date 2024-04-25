fb641e7554290d02fe56e8a01a3d0a2cd4720b57
Hajar Aswad <hajaraswadkom@gmail.com>
2024-04-25 17:16:02 +0700

    setup inertia

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('auth/login', 'login')->name('web.auth.login');
 });
